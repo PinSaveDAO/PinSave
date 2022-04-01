8d8145f3c99973f9a6ec6963ca75adaf8a639a43
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-04-01 19:53:08 +0300

    updated test mockup links

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -21,7 +21,8 @@ describe("PinSave", function () {
     describe("mint()", function () {
       it("Mint New NFT", async function () {
         const [owner] = await ethers.getSigners();
-        const sampleLink = "https://google.com/";
+        const sampleLink =
+          "https://bafkreiblu6yf35thyjzjhblimxiynxbewgn4dtjjozgjveuhrdmrfgx53a.ipfs.dweb.link/";
 
         await myContract.mintPost(owner.address, sampleLink);
         expect(await myContract.tokenURI(1)).to.equal(sampleLink);
@@ -30,7 +31,8 @@ describe("PinSave", function () {
       it("Minting should emit a Transfer event ", async function () {
         const [owner] = await ethers.getSigners();
 
-        const altLink = "https://dsfasdaa.com/ss";
+        const altLink =
+          "https://bafkreic5yqlwax3w46ugurxbbn2qlvqgdhkaiykqgxgrv2ozrqzjdzmipq.ipfs.dweb.link/";
 
         expect(await myContract.mintPost(owner.address, altLink))
           .to.emit(myContract, "Transfer")
