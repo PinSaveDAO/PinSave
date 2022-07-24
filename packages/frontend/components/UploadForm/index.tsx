b53f02879fca8c7156ec65c49d3b91324ee0aef0
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2022-07-24 19:23:08 +0300

    updated file field

--- packages/frontend/components/UploadForm/index.tsx
+++ packages/frontend/components/UploadForm/index.tsx
@@ -4,38 +4,70 @@ import {
   Title,
   TextInput,
   Textarea,
-  MantineTheme,
   Group,
-  useMantineTheme,
   Button,
+  Image,
   Center,
+  MediaQuery,
 } from "@mantine/core";
-import React from "react";
-import { Upload } from "tabler-icons-react";
+import React, { useState } from "react";
+import { Upload, Replace } from "tabler-icons-react";
 import { Dropzone, DropzoneStatus, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 export const dropzoneChildren = (
   status: DropzoneStatus,
-  theme: MantineTheme
-) => (
-  <Group
-    position="center"
-    spacing="xl"
-    style={{ minHeight: 220, pointerEvents: "none" }}
-  >
-    <Upload size={80} />
-
-    <div>
-      <Text size="xl" inline>
-        Drag image here or click to select a image
-      </Text>
-      <Text size="sm" color="dimmed" inline mt={7}>
-        Image should not exceed 5mb
-      </Text>
-    </div>
-  </Group>
-);
+  image: File | undefined
+) => {
+  if (image)
+    return (
+      <Group
+        position="center"
+        spacing="xl"
+        style={{ minHeight: 220, pointerEvents: "none" }}
+      >
+        <Image
+          src={URL.createObjectURL(image)}
+          alt=""
+          my="md"
+          radius="lg"
+          sx={{ maxWidth: "240px" }}
+        />
+        <Group sx={{ color: "#3a3a3a79" }}>
+          <MediaQuery
+            query="(max-width:500px)"
+            styles={{
+              marginLeft: "auto",
+              marginRight: "auto",
+              maxHeight: "30px",
+            }}
+          >
+            <Replace size={40} />
+          </MediaQuery>
+          <Text size="md" inline align="center">
+            Click/Drag here to replace image
+          </Text>
+        </Group>
+      </Group>
+    );
+  return (
+    <Group
+      position="center"
+      spacing="xl"
+      style={{ minHeight: 220, pointerEvents: "none" }}
+    >
+      <Upload size={80} />
+      <div>
+        <Text size="xl" inline>
+          Drag image here or click to select a image
+        </Text>
+        <Text size="sm" color="dimmed" inline mt={7}>
+          Image should not exceed 5mb
+        </Text>
+      </div>
+    </Group>
+  );
+};
 const UploadForm = () => {
-  const theme = useMantineTheme();
+  const [image, setImage] = useState<File | undefined>();
   return (
     <Paper
       withBorder
@@ -55,13 +87,14 @@ const UploadForm = () => {
       />
       <Dropzone
         mt="md"
-        onDrop={(files) => console.log("accepted files", files)}
-        onReject={(files) => console.log("rejected files", files)}
+        onDrop={(files) => setImage(files[0])}
         maxSize={3 * 1024 ** 2}
+        multiple={false}
         accept={IMAGE_MIME_TYPE}
       >
-        {(status) => dropzoneChildren(status, theme)}
+        {(status) => dropzoneChildren(status, image)}
       </Dropzone>
+
       <Center>
         <Button radius="lg" mt="md">
           Upload Post
