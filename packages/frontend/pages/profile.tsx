34ea986c16965d34bc748e56c24bda64828acb68
pfed-prog <fedorovn19@gmail.com>
2022-08-12 20:23:15 +0200

    added inspection of addresses

--- packages/frontend/pages/profile.tsx
+++ packages/frontend/pages/profile.tsx
@@ -1,70 +1,91 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { LSPFactory } from "@lukso/lsp-factory.js";
 import { useAccount } from "wagmi";
-
-const schema = [
-  {
-    name: "SupportedStandards:LSP3UniversalProfile",
-    key: "0xeafec4d89fa9619884b6b89135626455000000000000000000000000abe425d6",
-    keyType: "Mapping",
-    valueContent: "0xabe425d6",
-    valueType: "bytes",
-  },
-  {
-    name: "LSP3Profile",
-    key: "0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5",
-    keyType: "Singleton",
-    valueContent: "JSONURL",
-    valueType: "bytes",
-  },
-  {
-    name: "LSP1UniversalReceiverDelegate",
-    key: "0x0cfc51aec37c55a4d0b1a65c6255c4bf2fbdf6277f3cc0730c45b828b6db8b47",
-    keyType: "Singleton",
-    valueContent: "Address",
-    valueType: "address",
-  },
-];
-
-async function createUniversalProfile(publicAddress: string) {
-  const lspFactory = new LSPFactory("https://rpc.l16.lukso.network", {
-    deployKey: process.env.NEXT_PUBLIC_KEY,
-    chainId: 2828,
-  });
-
-  const deployedContracts = await lspFactory.UniversalProfile.deploy({
-    controllerAddresses: [publicAddress], // our EOA that will be controlling the UP
-    lsp3Profile: {
-      name: "My Universal Profile",
-      description: "My Cool Universal Profile",
-      tags: ["Public Profile"],
-      links: [
-        {
-          title: "My Website",
-          url: "https://my-website.com",
-        },
-      ],
-    },
-  });
-
-  return deployedContracts;
-}
+import LSP3UniversalProfileMetadata from "@erc725/erc725.js/schemas/LSP3UniversalProfileMetadata.json";
+import Web3 from "web3";
+import "isomorphic-fetch";
+import { ERC725, ERC725JSONSchema } from "@erc725/erc725.js";
 
 const Upload: NextPage = () => {
   const { address } = useAccount();
-  const [hasUP, setUP] = useState(false);
+  const [hasUP, setUP] = useState<undefined | string>("");
+  const [value, setValue] = useState("");
+  const [prData, setPrData] = useState("");
 
-  useEffect(() => {
-    if (address) {
-      const va = createUniversalProfile(address);
-      console.log(va);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log(`Form submitted, ${value}`);
+    fetchProfile(value);
+  };
+  async function fetchProfile(address: string) {
+    try {
+      const provider = new Web3.providers.HttpProvider(
+        "https://rpc.l16.lukso.network"
+      );
+      const config = {
+        ipfsGateway: "https://2eff.lukso.dev/ipfs/",
+      };
+
+      const erc725 = new ERC725(
+        LSP3UniversalProfileMetadata as ERC725JSONSchema[],
+        address,
+        provider,
+        config
+      );
+
+      const profileData = await erc725.fetchData();
+
+      const strPrData = JSON.stringify(profileData, undefined, 2);
+      //"0x79c5b2a5369872A4d5102022318ED62Fe7c0d065"
+      setPrData(strPrData);
+    } catch (error) {
+      return console.log("This is not an ERC725 Contract", error);
     }
-  }, [address]);
+  }
+
+  async function createUniversalProfile(publicAddress: string) {
+    const lspFactory = new LSPFactory("https://rpc.l16.lukso.network", {
+      deployKey: process.env.NEXT_PUBLIC_KEY,
+      chainId: 2828,
+    });
+    console.log("deploying");
+    const deployedContracts = await lspFactory.UniversalProfile.deploy({
+      controllerAddresses: [publicAddress], // our EOA that will be controlling the UP
+      lsp3Profile: {
+        name: "My Universal Profile",
+        description: "My Cool Universal Profile",
+        tags: ["Public Profile"],
+        links: [
+          {
+            title: "My Website",
+            url: "https://my.com",
+          },
+        ],
+      },
+    });
+
+    const myUPAddress = deployedContracts?.LSP0ERC725Account?.address;
+
+    console.log("my Universal Profile address: ", myUPAddress);
+    setUP(myUPAddress);
+
+    return deployedContracts;
+  }
+
   return (
-    <button
-      onClick={() => (address ? createUniversalProfile(address) : "")}
-    ></button>
+    <>
+      <button onClick={() => (address ? createUniversalProfile(address) : "")}>
+        create new account
+      </button>
+      <div>{hasUP}</div>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setValue(e.target.value)} value={value}></input>
+        <button type="submit">Inspect the Address</button>
+      </form>
+      <p>{prData}</p>
+    </>
   );
 };
 
