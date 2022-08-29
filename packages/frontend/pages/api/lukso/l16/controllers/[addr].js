15ca3084dae281ba522a8d8d218c767ff3470b9c
pfed-prog <fedorovn19@gmail.com>
2022-08-30 00:41:44 +0200

    fixed controller api

--- packages/frontend/pages/api/lukso/l16/controllers/[addr].js
+++ packages/frontend/pages/api/lukso/l16/controllers/[addr].js
@@ -2,41 +2,41 @@ import { ERC725 } from "@erc725/erc725.js";
 import LSP6Schema from "@erc725/erc725.js/schemas/LSP6KeyManager.json";
 import Web3 from "web3";
 
-const data = [];
-
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { addr } = request.query;
 
   const myUniversalProfileAddress = addr;
   const RPC_ENDPOINT = "https://rpc.l16.lukso.network";
 
   const web3 = new Web3(RPC_ENDPOINT);
+
   const erc725 = new ERC725(
     LSP6Schema,
     myUniversalProfileAddress,
     web3.currentProvider
   );
 
-  const result = erc725.getData("AddressPermissions[]");
+  const erc725Data = erc725.getData("AddressPermissions[]");
 
-  function getS(result) {
+  async function getS(result) {
+    const data = [];
     for (let ii = 0; ii < result.value.length; ii++) {
       const address = result.value[ii];
-      erc725
-        .getData({
-          keyName: "AddressPermissions:Permissions:<address>",
-          dynamicKeyParts: address,
-        })
-        .then((addressPermission) =>
-          erc725.decodePermissions(addressPermission.value)
-        )
-        .then((decodedPermission) =>
-          data.push({ decodedPermission, address: address })
-        );
+      const addressPermission = await erc725.getData({
+        keyName: "AddressPermissions:Permissions:<address>",
+        dynamicKeyParts: address,
+      });
+
+      const decodedPermission = erc725.decodePermissions(
+        addressPermission.value
+      );
+
+      data.push({ ...decodedPermission, address: address });
     }
+    return data;
   }
 
-  return result
+  return erc725Data
     .then((fetchedData) => getS(fetchedData))
-    .then(() => response.status(200).json({ data }));
+    .then((data) => response.status(200).json({ data }));
 }
