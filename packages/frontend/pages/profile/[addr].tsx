2b20561c2779ee93e71ec9dac21f1486fb5ce64a
pfed-prog <fedorovn19@gmail.com>
2022-09-03 18:37:24 +0200

    improved profile display

--- packages/frontend/pages/profile/[addr].tsx
+++ packages/frontend/pages/profile/[addr].tsx
@@ -1,25 +1,69 @@
-// import React, { useState, useEffect } from "react";
-//import { useRouter } from "next/router";
+import { Text, Title, Box } from "@mantine/core";
 
-const ProfilePage = ({ user }:{user:any}) => {
-  //const router = useRouter();
-  //const { addr } = router.query;
-
+const ProfilePage = ({ user }: { user: any }) => {
   return (
-    <div>
-      <pre>{JSON.stringify(user, null, 4)}</pre>
-    </div>
+    <Box
+      sx={(theme) => ({
+        display: "block",
+        backgroundColor:
+          theme.colorScheme === "dark"
+            ? theme.colors.dark[6]
+            : theme.colors.gray[0],
+        color:
+          theme.colorScheme === "dark"
+            ? theme.colors.blue[4]
+            : theme.colors.blue[7],
+        textAlign: "center",
+        padding: theme.spacing.xl,
+        borderRadius: theme.radius.md,
+        cursor: "pointer",
+
+        "&:hover": {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[5]
+              : theme.colors.gray[1],
+        },
+      })}
+    >
+      <Title align="center">{user.name}</Title>
+      <Text
+        component="span"
+        align="center"
+        variant="gradient"
+        gradient={{ from: "indigo", to: "cyan", deg: 45 }}
+        size="xl"
+        weight={700}
+        style={{ fontFamily: "Greycliff CF, sans-serif" }}
+      >
+        {user.description}
+      </Text>
+      <Title align="center" color="blue.5">
+        [{user.tags}]
+      </Title>
+    </Box>
   );
 };
 
-export async function getServerSideProps(context: { params: { addr: any; }; }) {
+export async function getServerSideProps(context: { params: { addr: any } }) {
   const { addr } = context.params;
-  const user = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT ?? "https://evm.pinsave.app/"}api/lukso/l14/${addr}`);
+  const user = await fetch(
+    `${
+      process.env.NEXT_PUBLIC_API_ENDPOINT ?? "https://evm.pinsave.app/"
+    }api/lukso/l14/${addr}`
+  );
   const data = await user.json();
 
   if (!data) {
     return {
       notFound: true,
+    };
+  }
+
+  if (data.ProfileData[1].name === "LSP3Profile") {
+    const profile = data.ProfileData[1].value.LSP3Profile;
+    return {
+      props: { user: profile },
     };
   }
 
