d65245077ff1424c1164f9f90026dc7a56c047e3
Seth McCullough <seth@endevrs.dev>
2022-09-16 20:57:10 -0400

    Replace router push with Link component

--- packages/frontend/components/Posts/PostCard.tsx
+++ packages/frontend/components/Posts/PostCard.tsx
@@ -1,12 +1,10 @@
 import Image from "next/future/image";
+import Link from "next/link";
 import { Paper, Text } from "@mantine/core";
-import { useRouter } from "next/router";
 
 import type { Post } from "@/services/upload";
 
 const PostCard = (post: Post) => {
-  const router = useRouter();
-
   let y;
   let x;
 
@@ -32,35 +30,37 @@ const PostCard = (post: Post) => {
   const imgSrc = `https://${x}`;
 
   return (
-    <Paper
-      onClick={() => router.push(`/polygon/posts/${post.token_id}`)}
-      withBorder
-      radius="lg"
-      shadow="md"
-      p="md"
-      sx={{ cursor: "pointer" }}
-    >
-      <div
-        style={{
-          position: "relative",
-          width: 200,
-          height: 200,
-        }}
+    <Link href={`/polygon/posts/${post.token_id}`} passHref>
+      <Paper
+        component="a"
+        withBorder
+        radius="lg"
+        shadow="md"
+        p="md"
+        sx={{ cursor: "pointer" }}
       >
-        <Image
-          src={imgSrc}
-          alt={post.name ? post.name : post._data?.name ?? "Image"}
-          placeholder="blur"
-          blurDataURL={imgSrc}
-          fill
-          sizes="200px"
-          style={{ objectFit: "cover" }}
-        />
-      </div>
-      <Text align="center" mt="sm">
-        {post.name ? post.name : post._data?.name}
-      </Text>
-    </Paper>
+        <div
+          style={{
+            position: "relative",
+            width: 200,
+            height: 200,
+          }}
+        >
+          <Image
+            src={imgSrc}
+            alt={post.name ? post.name : post._data?.name ?? "Image"}
+            placeholder="blur"
+            blurDataURL={imgSrc}
+            fill
+            sizes="200px"
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+        <Text align="center" mt="sm">
+          {post.name ? post.name : post._data?.name}
+        </Text>
+      </Paper>
+    </Link>
   );
 };
 
