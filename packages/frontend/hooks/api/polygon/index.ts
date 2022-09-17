3df116264d46f95da6e70d03b4a68b342fd88062
Seth McCullough <seth@endevrs.dev>
2022-09-16 20:24:59 -0400

    Add individual post query

--- packages/frontend/hooks/api/polygon/index.ts
+++ packages/frontend/hooks/api/polygon/index.ts
@@ -1,7 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { polygonKeys, fetchPolygonPosts } from "./queries";
+import { polygonKeys, fetchPolygonPosts, fetchPolygonPost } from "./queries";
 import type { Post } from "@/services/upload";
+
+type IndividualPost = Post & { owner: string };
 
 export const usePolygonPosts = () => {
   return useQuery<Post[]>(polygonKeys.all, () => fetchPolygonPosts());
+};
+
+export const usePolygonPost = (id: string) => {
+  return useQuery<IndividualPost>(polygonKeys.single(id), () =>
+    fetchPolygonPost(id)
+  );
 };
--- packages/frontend/hooks/api/polygon/queries.ts
+++ packages/frontend/hooks/api/polygon/queries.ts
@@ -2,8 +2,13 @@ import fetcher from "@/utils/fetcher";
 
 export const polygonKeys = {
   all: ["polygonPosts"] as const,
+  single: (id: string) => ["polygonPost", id] as const,
 };
 
 export const fetchPolygonPosts = async () => {
   return await fetcher("/api/polygon/posts/5");
+};
+
+export const fetchPolygonPost = async (id: string) => {
+  return await fetcher(`/api/polygon/post/${id}`);
 };
