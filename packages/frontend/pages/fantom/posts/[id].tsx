923bfe8816354b548194094c40bba65a5f276e7d
pfed-prog <fedorovn19@gmail.com>
2022-12-30 11:56:45 +0100

    added messaging

--- packages/frontend/pages/fantom/posts/[id].tsx
+++ packages/frontend/pages/fantom/posts/[id].tsx
@@ -4,6 +4,8 @@ import {
   SimpleGrid,
   Image,
   LoadingOverlay,
+  Button,
+  TextInput,
 } from "@mantine/core";
 import React, { useState, useEffect } from "react";
 
@@ -18,6 +20,10 @@ import { Orbis } from "@orbisclub/orbis-sdk";
 let orbis = new Orbis();
 
 const PostPage = () => {
+  const [user, setUser] = useState();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const router = useRouter();
   const currentChain = getCurrentChain(250);
   const { data: post, isLoading } = usePost(
@@ -25,7 +31,13 @@ const PostPage = () => {
     router.query.id as string
   );
 
-  const [user, setUser] = useState();
+  const sendMessage = async function () {
+    await orbis.createPost({
+      body: message,
+      context:
+        "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w",
+    });
+  };
 
   useEffect(() => {
     async function loadData() {
@@ -33,6 +45,47 @@ const PostPage = () => {
 
       if (res) {
         setUser(res);
+
+        /* let { data, error } = await orbis.getProfileGroups(res.did);
+        console.log(data); */
+
+        /* let { data, error } = await orbis.getGroup(
+          "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w"
+        ); */
+
+        let result = await orbis.getPosts({
+          context:
+            "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w",
+        });
+
+        setMessages(result.data);
+
+        /* let { data, error } = await orbis.getChannel(
+          "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w"
+        ); */
+
+        console.log(result.data);
+
+        /* await orbis.updateGroup(
+          "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w",
+          {
+            pfp: "https://evm.pinsave.app/PinSaveL.png",
+            name: "Pin Save",
+            description: "decentralised Pinterest",
+          }
+        ); */
+
+        //"kjzl6cwe1jw14ai2gg8e0qmx2j944ppe3s3dgfk003jlb8guuybyg4m77nsrg73"
+        /* await orbis.createChannel(
+          "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w",
+          {
+            group_id:
+              "kjzl6cwe1jw147hcck185xfdlrxq9zv0y0hoa6shzskqfnio56lhf8190yaei7w",
+            pfp: "https://evm.pinsave.app/PinSaveL.png",
+            name: "Pin Save",
+            description: "decentralised Pinterest",
+          }
+        ); */
       }
 
       if (!res) {
@@ -89,6 +142,36 @@ const PostPage = () => {
                   {post.owner}
                 </a>
               </p>
+
+              {messages &&
+                messages.map((post: any, i: number) => (
+                  <Paper
+                    shadow="xs"
+                    withBorder
+                    px="sm"
+                    sx={{ backgroundColor: "#80c7fc1d" }}
+                  >
+                    <p key={i}>{post.content.body}</p>
+                  </Paper>
+                ))}
+
+              <TextInput
+                my="lg"
+                required
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+                label="Message"
+                placeholder="Enter your message"
+                sx={{ maxWidth: "240px" }}
+              />
+              <Button
+                component="a"
+                radius="lg"
+                mt="md"
+                onClick={() => sendMessage()}
+              >
+                Upload Post
+              </Button>
             </Paper>
           </SimpleGrid>
         </>
