9343c5e385002da929beca66ec4138d3c50c94e4
pfed-prog <fedorovn19@gmail.com>
2023-01-17 12:35:09 +0100

    workingon profile

--- packages/frontend/pages/profile/index.tsx
+++ packages/frontend/pages/profile/index.tsx
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Orbis } from "@orbisclub/orbis-sdk";
+import { IconUsers, IconHeart } from "@tabler/icons";
+
 import {
+  BackgroundImage,
+  Box,
+  Button,
+  Card,
+  Center,
+  Container,
+  Group,
+  Image,
   Paper,
   Title,
+  Text,
   TextInput,
-  Button,
-  Image,
-  Center,
   LoadingOverlay,
 } from "@mantine/core";
 import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
@@ -18,9 +26,11 @@ import { dropzoneChildren } from "@/components/UploadForm";
 let orbis = new Orbis();
 
 const Upload = () => {
+  const [cover, setCover] = useState<string>();
   const [image, setImage] = useState<File | undefined>();
-
+  const [description, setDescription] = useState<string>();
   const [user, setUser] = useState<IOrbisProfile>();
+  const [followers, setFollowers] = useState<number>();
   const [username, setUsername] = useState<string>();
   const [pfp, setPfp] = useState<string>();
 
@@ -28,14 +38,14 @@ const Upload = () => {
     async function loadData() {
       let res = await orbis.isConnected();
 
-      if (res) {
-        setUser(res);
-      }
-
       if (!res) {
-        let res = await orbis.connect();
-        setUser(res);
+        res = await orbis.connect();
       }
+      setUser(res);
+      console.log(res);
+      /*       setCover(res.details.profile.cover);
+      setDescription(res.details.description);
+      setFollowers(res.details.count_followers); */
     }
     loadData();
   }, [user]);
@@ -94,37 +104,69 @@ const Upload = () => {
     <>
       {user && user.did ? (
         <>
+          <Button
+            my={12}
+            size="sm"
+            onClick={() => logout()}
+            style={{
+              float: "right",
+            }}
+          >
+            Log Out
+          </Button>
+          <Box sx={{ maxWidth: 1200 }} mx="auto">
+            <BackgroundImage
+              src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
+              radius="xs"
+              style={{
+                height: 500,
+                marginBottom: "25px",
+              }}
+            >
+              <Image
+                radius="md"
+                src={user.details.profile?.pfp}
+                alt={user.details.profile?.username}
+                mx="auto"
+                style={{
+                  width: 300,
+                  height: 300,
+                  paddingTop: 50,
+                }}
+              />
+
+              <Card
+                shadow="sm"
+                p="lg"
+                radius="lg"
+                withBorder
+                mx="auto"
+                style={{
+                  maxWidth: 400,
+                }}
+              >
+                <Center>
+                  <Title order={2}> {user.details.profile?.username} </Title>
+                </Center>
+                <Group mt={25} position="center">
+                  <Group position="center" mt="md" mb="xs">
+                    <IconUsers size={26} />
+                    <Text> {user.details.count_followers} </Text>
+                  </Group>
+                  <Container size={100}>
+                    <IconHeart size={20} />
+                  </Container>
+                </Group>
+              </Card>
+            </BackgroundImage>
+          </Box>
           <Paper
-            withBorder
             shadow="xl"
             p="md"
             radius="lg"
             sx={{ maxWidth: "700px", backgroundColor: "#82c7fc1d" }}
             mx="auto"
           >
-            <Button
-              my={12}
-              size="sm"
-              onClick={() => logout()}
-              style={{
-                float: "right",
-              }}
-            >
-              Log Out
-            </Button>
-            <div
-              style={{ width: 450, marginLeft: "auto", marginRight: "auto" }}
-            >
-              <Center>
-                <Title> {user.details.profile?.username} </Title>
-              </Center>
-              <Image
-                radius="md"
-                mt={10}
-                src={user.details.profile?.pfp}
-                alt={user.details.profile?.username}
-              />
-            </div>
             <TextInput
               my={12}
               size="md"
@@ -161,7 +203,6 @@ const Upload = () => {
             >
               {() => dropzoneChildren(image)}
             </Dropzone>
-
             <Center>
               <Button
                 my={12}
