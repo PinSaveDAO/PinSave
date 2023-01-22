04d2b094142af23cb3be8ad591621f1235eaffac
pfed-prog <fedorovn19@gmail.com>
2023-01-22 18:19:03 +0100

    updated improts

--- packages/foundry/test/Counter.t.sol
+++ packages/foundry/test/Counter.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
