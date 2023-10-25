7ff458ec0fd2e7135f791f96e78f104fceb90234
Pavel <fedorovn19@gmail.com>
2023-10-25 03:25:45 +0200

    specified type

--- packages/frontend/hooks/api/posts/index.ts
+++ packages/frontend/hooks/api/posts/index.ts
@@ -6,19 +6,20 @@ import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 export const usePosts = (chain: ChainName) => {
   return useInfiniteQuery(
     postKeys.byChain(chain),
-    ({ pageParam }) => fetchPosts(chain, { pageParam }),
+    ({ pageParam }: { pageParam?: number }) => fetchPosts(chain, { pageParam }),
     {
       getNextPageParam: (lastPage, pages) => {
         if (lastPage.items[5]?.token_id < lastPage.totalSupply) {
           return pages.length;
         }
+        return 0;
       },
-    },
+    }
   );
 };
 
 export const usePost = (chain: ChainName, id: string) => {
   return useQuery<IndividualPost>(postKeys.single(chain, id), () =>
-    fetchPost(chain, id),
+    fetchPost(chain, id)
   );
 };
