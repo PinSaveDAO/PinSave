ea0e7c527d61c88976c13d45a6167f2b882e087d
Pavel <fedorovn19@gmail.com>
2023-10-25 04:08:56 +0200

    fixing tanstack

--- packages/frontend/hooks/api/posts/index.ts
+++ packages/frontend/hooks/api/posts/index.ts
@@ -4,7 +4,7 @@ import type { IndividualPost } from "@/services/upload";
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 
 export const usePosts = (chain: ChainName) => {
-  return useInfiniteQuery(["posts", chain], ({ pageParam }) =>
+  return useInfiniteQuery(postKeys.byChain(chain), ({ pageParam }) =>
     fetchPosts(chain, { pageParam })
   );
 };
