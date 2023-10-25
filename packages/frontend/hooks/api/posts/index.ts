4a1d3024aefb820f8bac1dfb664221cdef0e28d8
Pavel <fedorovn19@gmail.com>
2023-10-25 05:33:20 +0200

    improving types

--- packages/frontend/hooks/api/posts/index.ts
+++ packages/frontend/hooks/api/posts/index.ts
@@ -5,21 +5,23 @@ import type { IndividualPost } from "@/services/upload";
 import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 
 export const usePosts = (chain: ChainName) => {
-  return useInfiniteQuery(
-    postKeys.byChain(chain),
-    ({ pageParam }) => fetchPosts(chain, { pageParam }),
-    {
-      getNextPageParam: (lastPage, pages) => {
-        if (lastPage.items[5]?.token_id < lastPage.totalSupply) {
-          return pages.length;
-        }
-      },
+  return useInfiniteQuery({
+    queryKey: [chain],
+    queryFn: async ({ pageParam }: { pageParam?: number | undefined }) => {
+      const data = await fetchPosts(chain, { pageParam });
+      return data;
     },
-  );
+
+    getNextPageParam: (lastPage: any, pages: any) => {
+      if (lastPage.items[5]?.token_id < lastPage.totalSupply) {
+        return pages.length;
+      }
+    },
+  });
 };
 
 export const usePost = (chain: ChainName, id: string) => {
   return useQuery<IndividualPost>(postKeys.single(chain, id), () =>
-    fetchPost(chain, id),
+    fetchPost(chain, id)
   );
 };
