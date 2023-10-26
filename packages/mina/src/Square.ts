5cd8cd6bc2e27f1cc95b0a67394af668d9e7833f
Pavel <fedorovn19@gmail.com>
2023-10-26 02:54:12 +0200

    added mina

---
+++ packages/mina/src/Square.ts
@@ -0,0 +1,24 @@
+import { 
+    Field, 
+    SmartContract, 
+    state, 
+    State, 
+    method 
+  } from 'o1js';
+
+  export class Square extends SmartContract {
+    @state(Field) num = State<Field>();
+  
+    init() {
+      super.init();
+      this.num.set(Field(3));
+    }
+
+    @method update(square: Field) {
+      const currentState = this.num.get();
+      this.num.assertEquals(currentState);
+      square.assertEquals(currentState.mul(currentState));
+      this.num.set(square);
+    }
+  }
+  
