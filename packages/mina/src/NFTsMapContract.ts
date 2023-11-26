63df19f5ac77a9638ebaa4a20fdfd7573eee31d1
pfed-prog <fedorovn19@gmail.com>
2023-11-26 08:42:11 +0200

    added second contract

---
+++ packages/mina/src/NFTsMapContract.ts
@@ -0,0 +1,56 @@
+import {
+  Field,
+  SmartContract,
+  state,
+  State,
+  method,
+  Permissions,
+  DeployArgs,
+  MerkleMapWitness,
+} from 'o1js';
+
+export class MerkleMapContract extends SmartContract {
+  @state(Field) treeRoot = State<Field>();
+
+  deploy(args?: DeployArgs) {
+    super.deploy(args);
+
+    const permissionToEdit = Permissions.proof();
+
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: permissionToEdit,
+      setTokenSymbol: permissionToEdit,
+      send: permissionToEdit,
+      receive: permissionToEdit,
+    });
+  }
+
+  @method initRoot(initialRoot: Field) {
+    this.treeRoot.assertEquals(Field.from(''));
+    this.treeRoot.set(initialRoot);
+  }
+
+  @method update(
+    keyWitness: MerkleMapWitness,
+    keyToChange: Field,
+    valueBefore: Field,
+    incrementAmount: Field
+  ) {
+    const initialRoot = this.treeRoot.getAndAssertEquals();
+
+    // check the initial state matches what we expect
+    const [rootBefore, key] = keyWitness.computeRootAndKey(valueBefore);
+
+    rootBefore.assertEquals(initialRoot);
+    key.assertEquals(keyToChange);
+
+    // compute the root after incrementing
+    const [rootAfter, _] = keyWitness.computeRootAndKey(
+      valueBefore.add(incrementAmount)
+    );
+
+    // set the new root
+    this.treeRoot.set(rootAfter);
+  }
+}
