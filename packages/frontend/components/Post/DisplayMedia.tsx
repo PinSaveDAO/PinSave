58d082c739833332ad623b0a148b593f68774f7f
pfed-prog <fedorovn19@gmail.com>
2023-12-11 13:50:48 +0100

    fixed image loading on posts; profile; fixed log out button display on profile

--- packages/frontend/components/Post/DisplayMedia.tsx
+++ packages/frontend/components/Post/DisplayMedia.tsx
@@ -2,7 +2,7 @@ import type { IndividualPost } from "@/services/upload";
 import { IsNotMp4 } from "@/utils/media";
 import VideoPlayer from "@/components/Post/VideoPlayer";
 
-import { Image } from "@mantine/core";
+import Image from "next/image";
 
 interface IMyProps {
   post: IndividualPost;
@@ -12,7 +12,17 @@ const DisplayMedia: React.FC<IMyProps> = ({ post }) => {
   return (
     <>
       {IsNotMp4(post?.image) ? (
-        <Image height={550} fit="contain" src={post.image} alt={post.name} />
+        <Image
+          height={600}
+          width={550}
+          src={post.image}
+          alt={post.name}
+          style={{
+            width: "99%",
+            height: "95%",
+            borderRadius: "10px",
+          }}
+        />
       ) : (
         <VideoPlayer {...post} />
       )}
