4bf0bb63f363230d3d942e50b5ec4b78f6c6a246
name <fedorovn19@gmail.com>
2024-03-17 21:59:22 +0100

    optimized card; added 1 sec to fade-in

--- packages/frontend/components/Posts/PostCard.tsx
+++ packages/frontend/components/Posts/PostCard.tsx
@@ -11,36 +11,31 @@ interface IMyProps {
 const PostCard: React.FC<IMyProps> = ({ post }) => {
   return (
     <Link href={`/posts/${post.id}`} className="fade-in">
-      <Center>
-        <Paper
-          component="div"
-          withBorder
-          radius="lg"
-          shadow="md"
-          p="md"
-          sx={{ cursor: "pointer" }}
-        >
-          <div
-            style={{
-              position: "relative",
-              height: 200,
-            }}
-          >
-            <Image
-              src={post.cid}
-              alt={post.name}
-              height={200}
-              width={200}
-              sizes="200px"
-              style={{ objectFit: "cover", borderRadius: "10px" }}
-              loading="lazy"
-            />
-          </div>
-          <Text align="center" mt="sm" lineClamp={1}>
-            {post.name}
-          </Text>
-        </Paper>
-      </Center>
+      <Paper
+        component="div"
+        withBorder
+        radius="lg"
+        shadow="md"
+        p="md"
+        sx={{ cursor: "pointer" }}
+      >
+        <Image
+          src={post.cid}
+          alt={post.name}
+          height={200}
+          width={200}
+          sizes="200px"
+          style={{
+            objectFit: "cover",
+            borderRadius: "10px",
+            width: "100%",
+          }}
+          loading="lazy"
+        />
+        <Text align="center" mt="sm" lineClamp={1}>
+          {post.name}
+        </Text>
+      </Paper>
     </Link>
   );
 };
